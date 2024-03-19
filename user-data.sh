9877a2a27147c5b52a2d06bb9157162f41ba0d91
MihirPashine <138210028+MIHIRPASHINE@users.noreply.github.com>
2024-03-19 13:04:00 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -5,4 +5,5 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+yum -y install git
+git clone https://github.com/Akiranred/food.git /var/www/html/
